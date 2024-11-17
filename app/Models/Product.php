b2bfd325856d389b853858f858e6b8858036760a
Wayne <aquinojwayne@gmail.com>
2024-11-18 00:23:31 +0800

    123

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'image',
+    ];
 }
