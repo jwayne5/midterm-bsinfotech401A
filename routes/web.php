6c6e0d3b6b137fb27922f3a27901e6bc64526915
Wayne <aquinojwayne@gmail.com>
2024-11-18 00:21:58 +0800

    123

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource("/products", ProductController::class);
+
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
