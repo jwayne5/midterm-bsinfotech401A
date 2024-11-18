56061f4c20c9ad64f3f410447999326b77200339
jwaynee <115685795+jwayne5@users.noreply.github.com>
2024-11-18 09:35:39 +0800

    123

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,48 +13,39 @@ class ProductController extends Controller
 
     public function index(): View
     {
-        $products = Product::all();
-        return view ('products.index')->with('products', $products);
+       
     }
 
  
     public function create(): View
     {
-        return view('products.create');
+        
     }
 
   
     public function store(Request $request): RedirectResponse
     {
-        $input = $request->all();
-        Product::create($input);
-        return redirect('products')->with('flash_message', 'Product Addedd!');
+        
     }
 
     public function show(string $id): View
     {
-        $products = Product::find($id);
-        return view('products.show')->with('products', $products);
+        
     }
 
     public function edit(string $id): View
     {
-        $products = Product::find($id);
-        return view('products.edit')->with('products', $products);
+        
     }
 
     public function update(Request $request, string $id): RedirectResponse
     {
-        $products = Product::find($id);
-        $input = $request->all();
-        $products->update($input);
-        return redirect('products')->with('flash_message', 'products Updated!');  
+       
     }
 
     
     public function destroy(string $id): RedirectResponse
     {
-        Product::destroy($id);
-        return redirect('products')->with('flash_message', 'products deleted!'); 
+       
     }
 }
