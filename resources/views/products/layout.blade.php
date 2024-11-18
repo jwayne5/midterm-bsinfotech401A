f6e9717b538759f5572d1a0ea90a46733b4827e6
reyss <reynaldo.abarro@cvsu.edu.ph>
2024-11-18 11:33:50 +0800

    ss

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -18,28 +18,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
-
-<script>
-    // Example of how you can populate the modal with product data dynamically
-    document.querySelectorAll('.btn-primary').forEach(function(button) {
-        button.addEventListener('click', function() {
-            // Get the product data from the button (or another data attribute)
-            var productId = this.getAttribute('data-id');
-            var productName = this.getAttribute('data-name');
-            var productPrice = this.getAttribute('data-price');
-            var productDescription = this.getAttribute('data-description');
-            var productImage = this.getAttribute('data-image');  // If you want to update the image as well
-
-            // Populate the modal with the product data
-            document.getElementById('product-id').value = productId;
-            document.getElementById('product-name').value = productName;
-            document.getElementById('product-price').value = productPrice;
-            document.getElementById('product-description').value = productDescription;
-            if (productImage) {
-                // Optionally, display the current image
-                document.getElementById('product-image').setAttribute('data-current-image', productImage);
-            }
-        });
-    });
-</script>
 </html>
