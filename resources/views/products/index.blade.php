033b6c9a1bb6662bd0d5453be185a185c16e8f88
jwaynee <115685795+jwayne5@users.noreply.github.com>
2024-11-18 10:58:30 +0800

    rollback

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -116,63 +116,7 @@
 
                                         
                                            
-                                            <div class="card-header">
-                                                <h1>Edit Product Page</h1>
-                                            </div>
-                                            <div class="card-body">
-                                                <form action="{{ url('products/' . $products->id) }}" method="post" enctype="multipart/form-data">
-                                                    {!! csrf_field() !!}
-                                                    @method("PATCH")
-                                                
-                                                    <!-- Hidden ID -->
-                                                    <input type="hidden" name="id" value="{{ $product->id }}" />
-                                                
-                                                    <!-- Product Name -->
-                                                    <label for="name">Name</label></br>
-                                                    <input type="text" name="name" id="name" value="{{ old('name', $product->name) }}" class="form-control"></br>
-                                                
-                                                    <!-- Product Price -->
-                                                    <label for="price">Price</label></br>
-                                                    <input type="text" name="price" id="price" value="{{ old('price', $product->price) }}" class="form-control"></br>
-                                                
-                                                    <!-- Product Description -->
-                                                    <label for="description">Description</label></br>
-                                                    <input type="text" name="description" id="description" value="{{ old('description', $product->description) }}" class="form-control"></br>
-                                                
-                                                    <!-- Product Image -->
-                                                    <label for="image">Image</label></br>
-                                                    <input type="file" name="image" id="image" class="form-control"></br>
-                                                
-                                                    @if($product->image)
-                                                        <div>
-                                                            <label>Current Image:</label>
-                                                            <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" width="100">
-                                                        </div>
-                                                    @endif
-                                                
-                                                    <br/>
-                                                    <input type="submit" value="Update" class="btn btn-success">
-                                                    <a href="{{ route('products.index') }}" class="btn btn-secondary">Cancel</a>
-                                                </form>
-                                            </div>
-
-                                            @foreach($products as $product)
-    <tr>
-        <td>{{ $product->id }}</td> <!-- Accessing the individual product's ID -->
-        <td>{{ $product->name }}</td>
-        <td>{{ $product->price }}</td>
-        <td>{{ $product->description }}</td>
-        <td>
-            @if($product->image)
-                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" width="100">
-            @endif
-        </td>
-        <td>
-            <a href="{{ url('products/' . $product->id) }}" class="btn btn-info btn-sm">View</a>
-            <a href="{{ url('products/' . $product->id . '/edit') }}" class="btn btn-primary btn-sm">Edit</a>
-        </td>
-    </tr>
-@endforeach
+                                            <a href="{{ url('/products/' . $item->id . '/edit') }}" title="Edit Products"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
  
                                             <form method="POST" action="{{ url('/products' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                                 {{ method_field('DELETE') }}
